1065129a848e6cce5912611c999b8fabe0ab5764
4sunny <sunghyoun.kim@mail.utoronto.ca>
2025-05-18 11:30:00 -0400

    feat: publish log with msg

--- middleware/internal/net/utils.go
+++ middleware/internal/net/utils.go
@@ -7,6 +7,18 @@ import (
 )
 
 func logHttpError(err error, msg string, statusCode int, w *http.ResponseWriter) {
-	log.Error().Err(err)
+	log.Error().Err(err).Msg(msg)
 	http.Error(*w, msg, statusCode)
+}
+
+func verifyRequest(r *http.Request, w *http.ResponseWriter) bool {
+	if r.Method != http.MethodPost {
+		logHttpError(nil, "Method not allowed", http.StatusMethodNotAllowed, w)
+		return false
+	}
+	if r.Header.Get("Content-Type") != "application/json" {
+		logHttpError(nil, "Unsupported Media Type", http.StatusUnsupportedMediaType, w)
+		return false
+	}
+	return true
 }
